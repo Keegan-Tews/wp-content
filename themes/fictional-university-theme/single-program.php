a3504f3c1a6c2938fa3dba646b562b3f28f9f8fe
Keegan Tews <keegan.tews@yahoo.com>
2024-02-09 10:52:54 -0800

    added the programs page

---
+++ themes/fictional-university-theme/single-program.php
@@ -0,0 +1,123 @@
+<?php
+// This is the template for individual program pages. It will display the content of the program post type. 
+
+
+get_header();
+
+    while (have_posts()) {
+        the_post(); ?>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>Replace later</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+    <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i>All Programs</a> <span class="metabox__main"><?php the_title(); ?></span>
+        </p>
+      </div>
+
+        <div class="generic-content">
+          <?php the_content(); ?>
+          <?php 
+
+         $relatedProfessors = new WP_Query(array(
+            'posts_per_page' => -1,
+            'post_type' => 'professor',
+            'orderby' => 'title',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'related_program',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"'
+              
+              )
+            )
+          ));
+
+          if ($relatedProfessors->have_posts()){
+            echo '<hr class="section-break">';
+            echo '<h2 class="headline headline--medium"> ' . get_the_title() . ' Professors</h2>';
+  
+            while($relatedProfessors->have_posts()) {
+              $relatedProfessors->the_post(); ?>
+              <li class="professor-card__list-item">
+                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+            </li>
+            <?php }
+
+          } wp_reset_postdata(); 
+
+
+
+
+
+
+          $today = date('Ymd');
+          $homepageEvents = new WP_Query(array(
+            'posts_per_page' => 2,
+            'post_type' => 'event',
+            'meta_key' => 'event_date',
+            'orderby' => 'meta_value_num',
+            'order' => 'ASC',
+            'meta_query' => array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              ),
+              array(
+                'key' => 'related_program',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"'
+              
+              )
+            )
+          ));
+
+          if ($homepageEvents->have_posts()){
+            echo '<hr class="section-break">';
+            echo '<h2>Upcoming ' . get_the_title() . ' Events</h2>';
+  
+            while($homepageEvents->have_posts()) {
+              $homepageEvents->the_post(); ?>
+              <div class="event-summary">
+              <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                <span class="event-summary__month">
+                  <?php $eventDate = new DateTime(get_field('event_date', false, false));
+                  echo $eventDate->format('M')
+                  ?>
+                </span>
+                <span class="event-summary__day">
+                  <?php echo $eventDate->format('d'); ?>
+                </span>
+              </a>
+              <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                <p><?php if (has_excerpt()) {
+                  echo get_the_excerpt();
+                } else {
+                  echo wp_trim_words(get_the_content(), 18);
+  
+                } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+              </div>
+            </div>
+            <?php }
+
+          }
+          wp_reset_postdata(); ?>
+
+    </div>
+
+    <?php
+    }
+get_footer();
+?>
