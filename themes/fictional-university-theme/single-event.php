a1ce7b504e36c31c4d1720f5b27b92fa7206089e
Keegan Tews <keegan.tews@yahoo.com>
2024-02-09 10:46:07 -0800

    Added the Event page and support single event post type

---
+++ themes/fictional-university-theme/single-event.php
@@ -0,0 +1,41 @@
+<?php
+get_header();
+
+    while (have_posts()) {
+        the_post(); ?>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>Replace later</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+      <div class="metabox metabox--position-up metabox--with-home-link">
+          <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i>Event Home</a> <span class="metabox__main"><?php the_title(); ?>
+          </span></p>
+        </div>
+
+          <div class="generic-content"><?php the_content(); ?></div>
+            <?php 
+            $relatedPrograms = get_field('related_program');
+
+            if ($relatedPrograms){
+              echo '<hr class="section-break">';
+              echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+              echo '<ul class="link-list min-list">'; 
+              foreach($relatedPrograms as $program) { ?>
+                <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+              <?php }
+              echo '</ul>';
+            }
+
+            ?>
+    </div>
+
+    <?php 
+  } get_footer();
+  ?>
