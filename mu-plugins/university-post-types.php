4172f682d08a841ebc3e72fb1f9250d5e8b00d5f
Keegan Tews <keegan.tews@yahoo.com>
2024-02-09 10:55:10 -0800

    added professor post type and a must use plugins file

---
+++ mu-plugins/university-post-types.php
@@ -0,0 +1,57 @@
+<?php
+function university_post_types() {
+    // Event Post Type
+    register_post_type('event', array(
+        'supports' => array('title', 'editor', 'excerpt'),
+        'rewrite' => array('slug' => 'events'),
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Events',
+            'add_new_item' => 'Add New Event',
+            'edit_item' => 'Edit Event',
+            'all_items' => 'All Events',
+            'singular_name' => 'Event'
+
+        ),
+        'menu_icon' => 'dashicons-calendar'
+      ));
+
+    // Program Post Type
+      register_post_type('program', array(
+        'supports' => array('title', 'editor'),
+        'rewrite' => array('slug' => 'programs'),
+        'has_archive' => true,
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Programs',
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => 'Program'
+
+        ),
+        'menu_icon' => 'dashiicons-awards'
+      ));
+
+      // Professor Post Type
+      register_post_type('professor', array(
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'public' => true,
+        'show_in_rest' => true,
+        'labels' => array(
+            'name' => 'Professors',
+            'add_new_item' => 'Add New Professor',
+            'edit_item' => 'Edit Professor',
+            'all_items' => 'All Professors',
+            'singular_name' => 'Professor'
+
+        ),
+        'menu_icon' => 'dashiicons-welcome-learn-more'
+      ));
+}
+
+add_action('init', 'university_post_types');
+?>
