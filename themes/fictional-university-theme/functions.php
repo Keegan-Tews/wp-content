ab6b04678182dfeeba30bf8d7410b6701ea21a5d
Keegan Tews <keegan.tews@yahoo.com>
2024-02-09 16:05:25 -0800

    Added image cropping and custome portrate and landscrape sizes

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -1,5 +1,35 @@
 <?php
 
+function pageBanner($args = NULL) {
+
+    if (!isset($args['title'])){
+        $args['title'] = get_the_title();
+    }
+
+    if (!isset($args['subtitle'])){
+        $args['subtitle'] = get_field('page_banner_subtitle');
+    }
+
+    if (!isset($args['photo'])){
+        if (get_field('page_banner_background_image') AND !is_archive() AND !is_home() ) {
+            $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+        } else {
+            $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+        }
+    }
+
+    ?>
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo'] ?>);"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+        <div class="page-banner__intro">
+          <p><?php echo $args['subtitle']; ?></p>
+        </div>
+      </div>
+    </div>
+
+    <?php }
 
 function university_files() {
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
@@ -14,6 +44,10 @@ add_action('wp_enqueue_scripts', 'university_files');
 function university_features() {
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_image_size('professorLandscape', 400, 260, true);
+    add_image_size('professorPortrait', 480, 650, true);
+    add_image_size('pageBanner', 1500, 350, true);
+
 }
 
 add_action('after_setup_theme', 'university_features');
--- themes/fictional-university-theme/single-program.php
+++ themes/fictional-university-theme/single-program.php
@@ -3,18 +3,10 @@
 
 
 get_header();
+pageBanner();
 
     while (have_posts()) {
         the_post(); ?>
-    <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
-      <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title"><?php the_title(); ?></h1>
-        <div class="page-banner__intro">
-          <p>Replace later</p>
-        </div>
-      </div>
-    </div>
 
     <div class="container container--narrow page-section">
     <div class="metabox metabox--position-up metabox--with-home-link">
@@ -46,12 +38,18 @@ get_header();
             echo '<hr class="section-break">';
             echo '<h2 class="headline headline--medium"> ' . get_the_title() . ' Professors</h2>';
   
+            echo '<ul class="professor-cards">';
             while($relatedProfessors->have_posts()) {
               $relatedProfessors->the_post(); ?>
               <li class="professor-card__list-item">
-                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                <a class="professor-card" href="<?php the_permalink(); ?>">
+                <img class="professor-card__image" src="<?php the_post_thumbnail_url('professorLandscape') ?>">
+                <span class="professor-card__name"><?php the_title(); ?></span>
+            
+                </a>
             </li>
             <?php }
+            echo '</ul>';
 
           } wp_reset_postdata(); 
 
@@ -88,29 +86,9 @@ get_header();
             echo '<h2>Upcoming ' . get_the_title() . ' Events</h2>';
   
             while($homepageEvents->have_posts()) {
-              $homepageEvents->the_post(); ?>
-              <div class="event-summary">
-              <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                <span class="event-summary__month">
-                  <?php $eventDate = new DateTime(get_field('event_date', false, false));
-                  echo $eventDate->format('M')
-                  ?>
-                </span>
-                <span class="event-summary__day">
-                  <?php echo $eventDate->format('d'); ?>
-                </span>
-              </a>
-              <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php if (has_excerpt()) {
-                  echo get_the_excerpt();
-                } else {
-                  echo wp_trim_words(get_the_content(), 18);
-  
-                } ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-              </div>
-            </div>
-            <?php }
+              $homepageEvents->the_post(); 
+              get_template_part('template-parts/content-event');
+               }
 
           }
           wp_reset_postdata(); ?>
